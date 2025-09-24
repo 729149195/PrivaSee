046ce64c21dbfaa8b32eb6130648415c6bfb1623
729149195 <729149195@qq.com>
2025-09-24 14:43:46 +0800

    init

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import { Divider } from 'antd';
 import styles from './Home.module.css'
-import TextTest from '../components/TextTest'
-import ImgTest from '../components/ImgTest'
-import AudioTest from '../components/AudioTest'
-import ConversationViz from '../components/ConversationViz'
 import AgentPage from '../components/AgentPage'
 
 export default function Home() {
